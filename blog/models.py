172ca5fcbab47f67820b0f357c5f5c8ec3d29ffd
kakutani <kakutani@aka.jp>
2017-03-08 18:13:54 +0900

    first commit

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    mail = models.EmailField()
+
+    # def __repr__(self):
+    #     # 主キーとnameを表示させて見やすくする
+    #     # ex) 1: Alice
+    #     return "{}: {}".format(self.pk, self.name)
+    #
+    # __str__ = __repr__  # __str__にも同じ関数を適用
+
+    # こっちのほうがよくね？
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    STATUS_DRAFT = "draft"
+    STATUS_PUBLIC = "public"
+    STATUS_SET = (
+            (STATUS_DRAFT, "下書き"),
+            (STATUS_PUBLIC, "公開中"),
+    )
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_SET, default=STATUS_DRAFT, max_length=8)
+    author = models.ForeignKey(User, related_name='entries')
+
+    def __str__(self):
+        return self.title
